f8d40cfe5e14704f98f5bc91988841b4d37f414c
Fabien Arcellier <fabien.arcellier@gmail.com>
2022-04-21 15:27:02 +0200

    alfred publish command triggers the workflow of publishing a version on pypi.
    * alfred publish configure a git tag using version in fixtup.__version__
    * alfred publish push the tag to github

--- alfred/publish.py
+++ alfred/publish.py
@@ -2,7 +2,22 @@ import os
 
 import alfred
 
+import fixtup
+
 ROOT_DIR = os.path.realpath(os.path.join(__file__, "..", ".."))
+
+
+@alfred.command("publish", help="tag a release of fixtup and release through github actions")
+def publish():
+    """
+    tag a release of fixtup and release through github actions
+
+    >>> $ alfred publish
+    """
+    git = alfred.sh("git", "git should be present")
+    os.chdir(ROOT_DIR)
+    alfred.run(git, ['tag', fixtup.__version__])
+    alfred.run(git, ['push', 'origin', fixtup.__version__])
 
 
 @alfred.command("publish:dist", help="build distribution packages")
